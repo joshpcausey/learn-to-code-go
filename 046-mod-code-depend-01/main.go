6126dcbe9127b0b87ab367cdb0e1b181b428172f
Joshua Causey <joshpcausey@gmail.com>
2025-08-22 14:10:41 -0500

    046-mod-code-depend-01

---
+++ 046-mod-code-depend-01/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt" // Import necessary packages
+
+	"github.com/joshpcausey/puppy"
+)
+
+func main() {
+	// Your program's logic goes here
+	fmt.Println(puppy.Bark())
+}
